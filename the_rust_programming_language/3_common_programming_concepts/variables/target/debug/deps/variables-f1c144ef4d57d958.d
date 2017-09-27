8a3f099f0a094c44aabb2cc052585ab299f87d76
Avi <avirzajac@gmail.com>
2017-09-27 18:47:59 -0400

    Starting part 3 run error currently

---
+++ the_rust_programming_language/3_common_programming_concepts/variables/target/debug/deps/variables-f1c144ef4d57d958.d
@@ -0,0 +1,5 @@
+/home/avi/Desktop/projects/Rust/the_rust_programming_language/3_common_programming_concepts/variables/target/debug/deps/variables-f1c144ef4d57d958: src/main.rs
+
+/home/avi/Desktop/projects/Rust/the_rust_programming_language/3_common_programming_concepts/variables/target/debug/deps/variables-f1c144ef4d57d958.d: src/main.rs
+
+src/main.rs:
